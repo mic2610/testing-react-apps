4ed5df331feed58fe84253b6033d0eca3e7512dc
Michael Ayoub <michael.ayoub@levo.com.au>
2024-07-24 12:08:34 +1000

    Exercises 1 - 5

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,20 +5,68 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import {expect, jest, test} from '@jest/globals';
+import { build, fake } from '@jackfranklin/test-data-bot';
 
-test('submitting the form calls onSubmit with username and password', () => {
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
+  let submittedData = null;
+
+  // const handleSubmit = (data) => (submittedData = data);
+
+  const handleSubmit = jest.fn();
+
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
   //
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
+  
+  const buildLoginForm = build({
+    fields: {
+      userNameText: fake(f => f.internet.userName()),
+      passwordText: fake(f => f.internet.password())
+    }
+  })
+  
+  // function buildLoginForm(overrides) {
+  //   return {
+  //     username: faker.internet.userName(),
+  //     password: faker.internet.password(),
+  //     ...overrides,
+  //   }
+  // }
+  
+
+  render(<Login onSubmit={handleSubmit} />);
+  
+  // let overridesUsername = 'test';
+  // const { userNameText, passwordText } = buildLoginForm({ fields : { userNameText: overridesUsername  }});
+  const { userNameText, passwordText } = buildLoginForm();
+  const username = screen.getByLabelText(/username/i);
+  const password = screen.getByLabelText(/password/i);
+  await userEvent.type(username, userNameText);
+  await userEvent.type(password, passwordText);
+
+  console.log('userNameText', userNameText);
+  //screen.debug();
   // 🐨 get the username and password fields via `getByLabelText`
   // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
+  //    whatever you wantw
   //
   // 🐨 click on the button with the text "Submit"
+  const submit = screen.getByRole('button', { name: /submit/i });
+  await userEvent.click(submit);
+
+  let expectedData = {
+    username: userNameText,
+    password: passwordText,
+  };
+  // expect(submittedData).toEqual();
+
+  // expect(overridesUsername).toEqual(userNameText);
+  expect(handleSubmit).toHaveBeenCalledWith(expectedData);
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
   //
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,11 +3,14 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
+import {handlers} from '../../test/server-handlers'
+import {rest} from 'msw'
 
 const buildLoginForm = build({
   fields: {
@@ -15,6 +18,8 @@ const buildLoginForm = build({
     password: fake(f => f.internet.password()),
   },
 })
+
+const server = setupServer(...handlers)
 
 // 🐨 get the server setup with an async function to handle the login POST request:
 // 💰 here's something to get you started
@@ -26,6 +31,9 @@ const buildLoginForm = build({
 // 📜 https://mswjs.io/
 
 // 🐨 before all the tests, start the server with `server.listen()`
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 // 🐨 after all the tests, stop the server with `server.close()`
 
 test(`logging in displays the user's username`, async () => {
@@ -35,14 +43,88 @@ test(`logging in displays the user's username`, async () => {
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 uncomment this and you'll start making the request!
-  // await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
   // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
   // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
 
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  screen.debug()
+  expect(screen.getByText(username)).toBeInTheDocument()
+
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
+})
+
+test(`logging in with no user's username returns UI error`, async () => {
+  render(<Login />)
+  const {password} = buildLoginForm()
+
+  //await userEvent.type(screen.getByLabelText(/username/i), null)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  // 🐨 uncomment this and you'll start making the request!
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  screen.debug()
+
+  expect(screen.getByRole('alert')).toMatchInlineSnapshot(`
+    <div
+      role="alert"
+      style="color: red;"
+    >
+      username required
+    </div>
+  `)
+})
+
+
+test('unknown server error displays an error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
+})
+
+test('server returns 404 not found when username not supplied', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  // server.use(
+  //   rest.post(
+  //     'https://auth-provider.example.com/api/login',
+  //     async (req, res, ctx) => {
+  //       return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+  //     },
+  //   ),
+  // );
+
+  const response = await fetch('https://auth-provider.example.com/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: null,
+      password: 'password'
+    }),
+  })
+
+  render(<Login />)
+  expect(response.status).toBe(400);
+  expect(response.statusText).toBe('Bad Request');
+
+  expect(await response.json()).toEqual({message: 'username required'});
 })
